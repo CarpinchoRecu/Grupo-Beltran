e6d0344a80c4dcdacc089146fa7f745d2502daae
FranciscoC <123523494+CarpinchoRecu@users.noreply.github.com>
2023-08-06 21:09:09 -0300

    configuracion del menuburger

--- grupobeltran/src/components/Header/Menuburger.jsx
+++ grupobeltran/src/components/Header/Menuburger.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const MenuBurger = () => {
@@ -19,6 +19,24 @@ const MenuBurger = () => {
     menuSpanRef.current.classList.toggle("hide");
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        offcanvasRef.current &&
+        !offcanvasRef.current.contains(event.target) &&
+        !menuBtnRef.current.contains(event.target)
+      ) {
+        handleClickClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div id="menu-burger">
       <div className="menu-btn" ref={menuBtnRef} onClick={handleClickMenu}>
@@ -26,19 +44,19 @@ const MenuBurger = () => {
       </div>
       <div className="offcanvas" ref={offcanvasRef}>
         <div className="close-btn" onClick={handleClickClose}></div>
-        <Link className="links" to="/Inicio">
+        <Link className="links" to="/Inicio" onClick={handleClickClose}>
           Inicio
         </Link>
-        <Link className="links" to="/Coberturas">
+        <Link className="links" to="/Coberturas" onClick={handleClickClose}>
           Coberturas
         </Link>
-        <Link className="links" to="/Nosotros">
+        <Link className="links" to="/Nosotros" onClick={handleClickClose}>
           Acerca de Nosotros
         </Link>
-        <Link className="links" to="/Contactanos">
+        <Link className="links" to="/Contactanos" onClick={handleClickClose}>
           Contáctanos
         </Link>
-        <Link className="links" to="/Trabajo">
+        <Link className="links" to="/Trabajo" onClick={handleClickClose}>
           Trabaja con Nosotros
         </Link>
       </div>
